89ed780814b45b85454a89581b11c1764a025dce
stackgen-automation <automation@stackgen.com>
2024-09-28 10:00:13 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -11,25 +11,25 @@ output "aws_cloudwatch_log_group_aws_lambda_rm_kitty_function_name" {
 }
 
 
-output "aws_lambda_rm_kitty_function_function_arn" {
+output "aws_lambda_rmkitty_function_function_arn" {
   value = module.appcd_67c8e755-1f4a-557b-a39f-be2a5de0ed51.function_arn
   sensitive = false
 }
 
 
-output "aws_lambda_rm_kitty_function_function_name" {
+output "aws_lambda_rmkitty_function_function_name" {
   value = module.appcd_67c8e755-1f4a-557b-a39f-be2a5de0ed51.function_name
   sensitive = false
 }
 
 
-output "aws_lambda_rm_kitty_function_function_url" {
+output "aws_lambda_rmkitty_function_function_url" {
   value = module.appcd_67c8e755-1f4a-557b-a39f-be2a5de0ed51.function_url
   sensitive = false
 }
 
 
-output "aws_lambda_rm_kitty_function_invoke_arn" {
+output "aws_lambda_rmkitty_function_invoke_arn" {
   value = module.appcd_67c8e755-1f4a-557b-a39f-be2a5de0ed51.invoke_arn
   sensitive = false
 }
